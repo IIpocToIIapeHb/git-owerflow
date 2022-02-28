afa89198e3851ac7ff57972366e80db04809c8c5
KpacuBbIuIIapeHb <kostiarudoy2303@gmail.com>
2022-02-28 23:18:12 +0300

    fife

--- src/by/home/git/HelloWorld.java
+++ src/by/home/git/HelloWorld.java
@@ -3,6 +3,9 @@ package by.home.git;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Жыве БЕларусь");
+		
+		
+		
 		System.out.println("Нет войне");
 	}
 
