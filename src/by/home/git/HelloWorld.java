0d08926881f2710864db5fa1ebe82846db3aac05
KpacuBbIuIIapeHb <kostiarudoy2303@gmail.com>
2022-03-01 21:18:48 +0300

    12 commit

--- src/by/home/git/HelloWorld.java
+++ src/by/home/git/HelloWorld.java
@@ -4,6 +4,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Жыве БЕларусь");
 	
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		System.out.println("Нет войне");
 	}
 
